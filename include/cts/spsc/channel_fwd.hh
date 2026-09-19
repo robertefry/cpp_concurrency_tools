4d895732ba57ff8e1d489e56f7ff68b08ca792f8
Robert Fry <inbox@robertfry.xyz>
2026-09-19 06:14:05 +0100

    refact: array_channel.hh

---
+++ include/cts/spsc/channel_fwd.hh
@@ -0,0 +1,15 @@
+
+#ifndef CTS_SPSC_CHANNEL_FWD_HH
+#define CTS_SPSC_CHANNEL_FWD_HH
+
+namespace cts::spsc {
+
+    template <typename T, typename Channel>
+    class Sender;
+
+    template <typename T, typename Channel>
+    class Receiver;
+
+} // namespace cts::spsc
+
+#endif /* CTS_SPSC_CHANNEL_FWD_HH */
